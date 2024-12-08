291726ba3f1d6811202bb1aa464bd65a78b84ba5
philokaram <philokrm@gmail.com>
2024-12-08 18:05:00 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -52,6 +52,10 @@ public class Main {
                 System.out.println("--------------------------");
                 int time =periods.get(0).process.arrivalTime;
                 for (processPeriod processPeriod : periods) {
+                    if(processPeriod.process.arrivalTime > time ){
+                        System.out.println(time+" --> "+ processPeriod.process.arrivalTime +" no process in CPU");
+                        time =processPeriod.process.arrivalTime;
+                    }
                     System.out.println(
                             time + " --> " + (time + processPeriod.period) + " - " + processPeriod.process.name);
                     time += processPeriod.period;
@@ -88,6 +92,10 @@ public class Main {
                 System.out.println("--------------------------");
                 int time =periods.get(0).process.arrivalTime;
                 for (processPeriod processPeriod : periods) {
+                    if(processPeriod.process.arrivalTime > time ){
+                        System.out.println(time+" --> "+ processPeriod.process.arrivalTime +" no process in CPU");
+                        time =processPeriod.process.arrivalTime;
+                    }
                     System.out.println(
                             time + " --> " + (time + processPeriod.period) + " - " + processPeriod.process.name);
                     time += processPeriod.period;
@@ -126,6 +134,10 @@ public class Main {
                 System.out.println("--------------------------");
                 int time =periods.get(0).process.arrivalTime;
                 for (processPeriod processPeriod : periods) {
+                    if(processPeriod.process.arrivalTime > time ){
+                        System.out.println(time+" --> "+ processPeriod.process.arrivalTime +" no process in CPU");
+                        time =processPeriod.process.arrivalTime;
+                    }
                     System.out.println(
                             time + " --> " + (time + processPeriod.period) + " - " + processPeriod.process.name);
                     time += processPeriod.period;
